7897592dd8d03216cb00eee53dfd1091d9471759
Bielmfp18 <gabrielmartins180706@gmail.com>
2025-02-06 16:12:24 -0300

    Arquivo de conexão para o Banco de Dados concluído.

--- conn/connect.php
+++ conn/connect.php
@@ -1,5 +1,22 @@
 <?php 
 
+$host = "127.0.0.1"; //Ou localhost
+$database = "tinsphpdb01";
+$root = "root";
+$pass = "";
+$charset = "utf8";
+$port = "3306";
 
-
+//Todo bloco "try" termina com "catch".
+try{
+    //Lembre dessa variável quando usar um comando SQL no PHP.
+    //"$conn" é um objeto da classe mysqli do tipo SQL.
+    $conn = new mysqli($host, $root, $pass, $database, $port);
+    //Declarando o utf8 através da variável "$charset".
+    mysqli_set_charset($conn, $charset);
+}
+//Throwable é um objeto de excessão, a variável "$th" vai receber todos dados que deram erro durante o código.
+catch(Throwable $th){
+    die("Atenção rolou um ERRO, Cara! ".$th);
+}
 ?>
