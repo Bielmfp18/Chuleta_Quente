2f02237dc13ae6d3b69d6f06d99125b5f3bee5ad
Bielmfp18 <gabrielmartins180706@gmail.com>
2025-02-06 14:36:50 -0300

    Adicionando a tag php e html

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-<?php ?>
+<?php 
+#
+//
+/* Modelo projeto */
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>modelophp</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+!
