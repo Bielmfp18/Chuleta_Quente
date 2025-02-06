2f9ff6031611df18cc9c789728bdde95e8378956
Bielmfp18 <gabrielmartins180706@gmail.com>
2025-02-06 17:27:14 -0300

    Preparação do ambiente e index

--- index.php
+++ index.php
@@ -12,9 +12,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/estilo.css">
-    <title>Modelophp</title>
+    <title>Chuleta Quente Churrascaria</title>
 </head>
-<body>
-    
+<body class = "fundofixo">
+    <!-- Área de menu -->
+     <?php include 'menu_publico.php'; ?>
+     <a name="home">&nbsp;</a>
+     <main class="container">
+    <!-- Área de Carrossel -->
+     <?php include 'carrossel.php';?>
+     <!-- Área de Destaque -->
+      <a class="pt-6" name = "destaques">&nbsp;</a>
+      <?php include 'produtos_destque.php';?>
+     <!--Área geral de produtos -->
+     <a class="pt-6" name = "produtos">&nbsp;</a>
+     <!-- "include" pega todas as informações do arquivo e o adiciona ao local declarado no código html -->
+     <?php include 'produtos_geral.php';?>
+     <!--  Rodapé -->
+       <footer class="panel-footer"></footer>
+       <?php include 'rodape.php';?>
+       <a name="contato"></a>
+    </main>
 </body>
 </html>
