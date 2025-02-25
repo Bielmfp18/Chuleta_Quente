2d3ed455ebde0551737deb0ff38fc09147679b35
GABRIEL MARTINS FORTUNATO DE PAULA <gabrielmartins180706@gmail.com>
2025-02-24 23:54:32 -0300

    Salvando últimas mudanças

--- produtos_busca.php
+++ produtos_busca.php
@@ -16,24 +16,23 @@ $numLinhas = $lista_busca->num_rows;
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/estilo.css">
     <title>Busca por palavra</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 </head>
  
 <body class="fundofixo">
-    <?php
-    include 'menu_publico.php';
-    ?>
+    <?php include 'menu_publico.php'; ?>
     <div class="container">
-        <!-- monstrar se a consulta retornar vazio -->
+        <!-- Mostrar se a consulta retornar vazio -->
         <?php if ($numLinhas == 0) { ?>
             <h2 class="breadcrumb alert-danger">
                 <a href="javascript:window.history.go(-1)" class="btn btn-danger">
                     <span class="glyphicon glyphicon-chevron-left"></span>
                 </a>
-                Não há produtos relacionados com <strong> "<?php echo $busca ?>"</strong>
+                Não há produtos relacionados com <strong>"<?php echo $busca ?>"</strong>
             </h2>
         <?php } ?>
-        <!-- mostrar se a consulta retornou produtos  -->
-        <?php if ($numLinhas > 1) { ?>
+        <!-- Mostrar se a consulta retornou produtos -->
+        <?php if ($numLinhas > 0) { ?>
             <h2 class="breadcrumb alert-danger">
                 <a href="javascript:window.history.go(-1)" class="btn btn-danger">
                     <span class="glyphicon glyphicon-chevron-left"></span>
@@ -55,45 +54,24 @@ $numLinhas = $lista_busca->num_rows;
                                     <strong><?php echo $linhaBusca['rotulo']; ?></strong>
                                 </p>
                                 <p class="text-left">
-                                    <?php echo mb_strimwidth($linhaBusca['resumo'], 0, 42, '...') ?>
+                                    <?php echo mb_strimwidth($linhaBusca['resumo'], 0, 42, '...'); ?>
                                 </p>
                                 <p>
                                     <button class="btn btn-default disabled" role="button" style="cursor:default;">
-                                    <?php echo "R$ ".number_format($linhaBusca['valor'],2,',','.');?>
+                                        <?php echo "R$ ".number_format($linhaBusca['valor'],2,',','.'); ?>
                                     </button>
-                                    <a href="produto_detalhes.php?id=<?php echo $linhaBusca['id_tipo']; ?>">
+                                    <a href="produto_detalhes.php?id=<?php echo $linhaBusca['tipo_id']; ?>">
                                         <span class="hidden-xs">Saiba mais...</span>
                                         <span class="hidden-xs glyphicon glyphicon-eye-open" aria-hidden="true"></span>
                                     </a>
                                 </p>
                             </div>
- 
- 
                         </div>
                     </div>
-                <?php } while ($linhaBusca = $lista_busca->fetch_assoc()) ?>
+                <?php } while ($linhaBusca = $lista_busca->fetch_assoc()); ?>
             </div>
         <?php } ?>
- 
     </div>
- 
- 
- 
- 
 </body>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
-<script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
-<script type="text/javascript">
-    $(document).on('ready', function() {
-        $(".regular").slick({
-            dots: true,
-            infinity: true,
-            slidesToShow: 3,
-            slidesToScroll: 3
-        });
-    });
-</script>
-<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick/slick.min.js"></script>
- 
-</html>
