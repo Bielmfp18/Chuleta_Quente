0638f91b0d8b1ad2ac55128f1a0c0e49fb7bc30a
Gabriel Martins Fortunato de Paula <gabrielmartins180706@gmail.com>
2025-06-09 18:45:35 -0300

    Salvando denovo

--- conn/connect.php
+++ conn/connect.php
@@ -1,9 +1,9 @@
 <?php 
 
-$host = "sql102.infinityfree.com"; //Ou localhost
-$database = "if0_39054069_chuleta_quente";
-$user = "if0_39054069";
-$pass = "pPWamFJPmaUX";
+$host = "127.0.0.1"; //Ou localhost
+$database = "tinsphpdb01";
+$root = "root";
+$pass = "";
 $charset = "utf8";
 $port = "3306";
 
@@ -11,16 +11,12 @@ $port = "3306";
 try{
     //Lembre dessa variável quando usar um comando SQL no PHP.
     //"$conn" é um objeto da classe mysqli do tipo SQL.
-    $conn = new mysqli($host, $user, $pass, $database, $port);
+    $conn = new mysqli($host, $root, $pass, $database, $port);
     //Declarando o utf8 através da variável "$charset".
     mysqli_set_charset($conn, $charset);
-   
-    //Teste para a conexão com Banco de Dados.
-    # echo "connect right";
 }
 //Throwable é um objeto de excessão, a variável "$th" vai receber todos dados que deram erro durante o código.
 catch(Throwable $th){
     die("Atenção rolou um ERRO, Cara! ".$th);
 }
 ?>
-
